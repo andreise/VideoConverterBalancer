41c12ccf5513d08c078c20757f9f359666a25f02
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-07-29 13:31:50 +0400

    Moved MQConsts into partial class container

--- VideoConverter.Common/VideoConverterApplicationBase.cs
+++ VideoConverter.Common/VideoConverterApplicationBase.cs
@@ -7,30 +7,8 @@ using System.Threading.Tasks;
 
 namespace VideoConverter.Common
 {
-    public abstract class VideoConverterApplicationBase
+    public abstract partial class VideoConverterApplicationBase
     {
-        protected static class MQConsts
-        {
-            public static class ExchangeTypes
-            {
-                public const string Fanout = "fanout";
-            }
-
-            public static class Exchanges
-            {
-                public const string VideoConverterDefault = "video_converter";
-            }
-
-            public static class Queues
-            {
-                public const string IsCompleted = "is_completed";
-                public const string FreeFiles = "free_files";
-                public const string FilesProcessingStarted = "files_processing_started";
-                public const string FilesProcessedSuccessfully = "files_processed_successfully";
-                public const string FilesFailed = "file_failed";
-            }
-        }
-
         private static Encoding SerializationEncoding => Encoding.UTF8;
 
         protected static byte[] SerializeFile(ConverterFileInfo file) =>
---
+++ VideoConverter.Common/VideoConverterApplicationBase_MQConsts.cs
@@ -0,0 +1,27 @@
+﻿namespace VideoConverter.Common
+{
+    partial class VideoConverterApplicationBase
+    {
+        protected static class MQConsts
+        {
+            public static class ExchangeTypes
+            {
+                public const string Fanout = "fanout";
+            }
+
+            public static class Exchanges
+            {
+                public const string VideoConverterDefault = "video_converter";
+            }
+
+            public static class Queues
+            {
+                public const string IsCompleted = "is_completed";
+                public const string FreeFiles = "free_files";
+                public const string FilesProcessingStarted = "files_processing_started";
+                public const string FilesProcessedSuccessfully = "files_processed_successfully";
+                public const string FilesFailed = "file_failed";
+            }
+        }
+    }
+}
